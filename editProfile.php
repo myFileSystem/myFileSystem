5893e54598c69b59b6339d4a82c14f1388947439
Squall Dai <squall.dai@gmail.com>
2012-02-07 17:02:17 +0800

    editProfile ln7

--- editProfile.php
+++ editProfile.php
@@ -4,6 +4,7 @@ $thisprog = "editProfile.php";
 require ("global.php");
 echo "<title>$title /$xtm</title>";
 //test
+//test2
 $link = mysql_connect('127.0.0.1', 'root', 'squall') or die('Could not connect: ' . mysql_error());
 mysql_select_db('app') or die('Could not select database');
 $query = "SELECT * FROM user WHERE userName='$_SESSION[userName]'";
