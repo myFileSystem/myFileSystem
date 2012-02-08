3673084c02790207c2dae56a0dfb50b18554103d
Squall Dai <squall.dai@gmail.com>
2012-02-08 16:14:56 +0800

    sql doc

--- SQL documents/sitemessage.sql
+++ SQL documents/sitemessage.sql
@@ -1,5 +1,40 @@
-INSERT INTO `sitemessage` (`id`, `messageFrom`, `messageTo`, `messageTitle`, `messageContent`, `messageTime`, `isFromSys`, `isRead`) VALUES (1, 3, 1, 'test', 'this is a test', '2012-3-6', '0', '0');
-INSERT INTO `sitemessage` (`id`, `messageFrom`, `messageTo`, `messageTitle`, `messageContent`, `messageTime`, `isFromSys`, `isRead`) VALUES (2, 1, 3, 'test2', 'this is a test', '2012-2-1', '1', '0');
-INSERT INTO `sitemessage` (`id`, `messageFrom`, `messageTo`, `messageTitle`, `messageContent`, `messageTime`, `isFromSys`, `isRead`) VALUES (5, 2, 3, 'seifer to squall new', 'seifer to squall new', '2012-2-8', '0', '0');
-INSERT INTO `sitemessage` (`id`, `messageFrom`, `messageTo`, `messageTitle`, `messageContent`, `messageTime`, `isFromSys`, `isRead`) VALUES (9, 3, 1, 'sysmes from squall', 'sysmes from squall', '2012-2-8', '1', '0');
-INSERT INTO `sitemessage` (`id`, `messageFrom`, `messageTo`, `messageTitle`, `messageContent`, `messageTime`, `isFromSys`, `isRead`) VALUES (10, 3, 2, 'sysmes from squall', 'sysmes from squall', '2012-2-8', '1', '0');
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : root
+Source Server Version : 60000
+Source Host           : localhost:3306
+Source Database       : app
+
+Target Server Type    : MYSQL
+Target Server Version : 60000
+File Encoding         : 65001
+
+Date: 2012-02-08 16:14:29
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `sitemessage`
+-- ----------------------------
+DROP TABLE IF EXISTS `sitemessage`;
+CREATE TABLE `sitemessage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `messageFrom` bigint(20) NOT NULL,
+  `messageTo` bigint(20) NOT NULL,
+  `messageTitle` varchar(30) NOT NULL,
+  `messageContent` varchar(200) CHARACTER SET gb2312 NOT NULL,
+  `messageTime` date NOT NULL,
+  `isFromSys` char(1) NOT NULL DEFAULT '0',
+  `isRead` char(1) CHARACTER SET gb2312 NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sitemessage
+-- ----------------------------
+INSERT INTO `sitemessage` VALUES ('1', '3', '1', 'test', 'this is a test', '2012-03-06', '0', '0');
+INSERT INTO `sitemessage` VALUES ('2', '1', '3', 'test2', 'this is a test', '2012-02-01', '1', '0');
+INSERT INTO `sitemessage` VALUES ('5', '2', '3', 'seifer to squall new', 'seifer to squall new', '2012-02-08', '0', '0');
+INSERT INTO `sitemessage` VALUES ('9', '3', '1', 'sysmes from squall', 'sysmes from squall', '2012-02-08', '1', '0');
+INSERT INTO `sitemessage` VALUES ('10', '3', '2', 'sysmes from squall', 'sysmes from squall', '2012-02-08', '1', '0');
--- SQL documents/user.sql
+++ SQL documents/user.sql
@@ -1,3 +1,39 @@
-INSERT INTO `user` (`id`, `userName`, `password`, `chineseName`, `company`, `phoneNumber`, `userRight`, `secretKey`, `emailAddr`) VALUES (1, 'rinoa', 'd41d8cd98f00b204e9800998ecf8427e', 'chine', 'checkit', '1865000000', 'low', 'd41d8cd98f00b204e9800998ecf8427e', 'rinoa@hotmail');
-INSERT INTO `user` (`id`, `userName`, `password`, `chineseName`, `company`, `phoneNumber`, `userRight`, `secretKey`, `emailAddr`) VALUES (2, 'seifer', '493603d84f0098e827896d5c08c81480', 'seifertest', 'seifer', '444444444', 'low', '493603d84f0098e827896d5c08c81480', 'seifer@hotmail.com');
-INSERT INTO `user` (`id`, `userName`, `password`, `chineseName`, `company`, `phoneNumber`, `userRight`, `secretKey`, `emailAddr`) VALUES (3, 'squall', 'f9ba7d6af07eecbbccd33510304aba1b', 'chine', 'VPT-TECH', '1555555555', 'super', 'secretKey1', 'squall@gmail');
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : root
+Source Server Version : 60000
+Source Host           : localhost:3306
+Source Database       : app
+
+Target Server Type    : MYSQL
+Target Server Version : 60000
+File Encoding         : 65001
+
+Date: 2012-02-08 16:14:37
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(10) NOT NULL DEFAULT '',
+  `password` char(32) NOT NULL,
+  `chineseName` varchar(20) CHARACTER SET gb2312 DEFAULT NULL,
+  `company` varchar(50) DEFAULT NULL,
+  `phoneNumber` varchar(20) DEFAULT NULL,
+  `userRight` varchar(20) DEFAULT NULL,
+  `secretKey` varchar(32) DEFAULT NULL,
+  `emailAddr` varchar(30) CHARACTER SET gb2312 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'rinoa', 'd41d8cd98f00b204e9800998ecf8427e', 'chine', 'checkit', '1865000000', 'low', 'd41d8cd98f00b204e9800998ecf8427e', 'rinoa@hotmail');
+INSERT INTO `user` VALUES ('2', 'seifer', '493603d84f0098e827896d5c08c81480', 'seifertest', 'seifer', '444444444', 'low', '493603d84f0098e827896d5c08c81480', 'seifer@hotmail.com');
+INSERT INTO `user` VALUES ('3', 'squall', 'f9ba7d6af07eecbbccd33510304aba1b', 'chine', 'VPT-TECH', '1555555555', 'super', 'secretKey1', 'squall@gmail');
