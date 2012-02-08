75a7ea1910ae922200681c963afa438473fe46fd
unknown <Administrator@PC-201007051035.(none)>
2012-02-08 19:19:37 +0800

    Signed-off-by: unknown <Administrator@PC-201007051035.(none)>

---
+++ SQL documents/sitemessage1.sql
@@ -0,0 +1,48 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 2.10.0.2
+-- http://www.phpmyadmin.net
+-- 
+-- 主机: localhost
+-- 生成日期: 2012 年 02 月 08 日 11:17
+-- 服务器版本: 5.5.20
+-- PHP 版本: 5.2.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+-- 
+-- 数据库: `app`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- 表的结构 `sitemessage`
+-- 
+
+CREATE TABLE `sitemessage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `messageFrom` bigint(20) NOT NULL,
+  `messageTo` bigint(20) NOT NULL,
+  `messageTitle` varchar(30) NOT NULL,
+  `messageContent` varchar(200) CHARACTER SET gb2312 NOT NULL,
+  `messageTime` date NOT NULL,
+  `isFromSys` char(1) NOT NULL DEFAULT '0',
+  `isRead` char(1) CHARACTER SET gb2312 NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+
+-- 
+-- 导出表中的数据 `sitemessage`
+-- 
+
+INSERT INTO `sitemessage` (`id`, `messageFrom`, `messageTo`, `messageTitle`, `messageContent`, `messageTime`, `isFromSys`, `isRead`) VALUES 
+(1, 3, 1, 'test', 'this is a test', '2012-03-06', '0', '0'),
+(2, 1, 3, 'test2', 'this is a test', '2012-02-01', '1', '0'),
+(5, 2, 3, 'seifer to squall new', 'seifer to squall new', '2012-02-08', '0', '0'),
+(9, 3, 1, 'sysmes from squall', 'sysmes from squall', '2012-02-08', '1', '0'),
+(10, 3, 2, 'sysmes from squall', 'sysmes from squall', '2012-02-08', '1', '0'),
+(12, 6, 7, 'hello', 'this is a test', '2012-03-16', '1', '0'),
+(13, 8, 9, 'HI', 'this is a test', '2012-12-01', '1', '1'),
+(14, 9, 1, 'nihaoma', 'wegggh', '2012-04-08', '0', '1'),
+(15, 4, 5, 'wohenhao', 'eeee', '2012-06-08', '1', '1'),
+(17, 4, 3, 'sysmes from meimie', 'hahha', '2012-08-08', '1', '1');
---
+++ SQL documents/user1.sql
@@ -0,0 +1,48 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 2.10.0.2
+-- http://www.phpmyadmin.net
+-- 
+-- 主机: localhost
+-- 生成日期: 2012 年 02 月 08 日 11:17
+-- 服务器版本: 5.5.20
+-- PHP 版本: 5.2.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+-- 
+-- 数据库: `app`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- 表的结构 `user`
+-- 
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(10) NOT NULL DEFAULT '',
+  `password` char(32) NOT NULL,
+  `chineseName` varchar(20) CHARACTER SET gb2312 DEFAULT NULL,
+  `company` varchar(50) DEFAULT NULL,
+  `phoneNumber` varchar(20) DEFAULT NULL,
+  `userRight` varchar(20) DEFAULT NULL,
+  `secretKey` varchar(32) DEFAULT NULL,
+  `emailAddr` varchar(30) CHARACTER SET gb2312 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+-- 
+-- 导出表中的数据 `user`
+-- 
+
+INSERT INTO `user` (`id`, `userName`, `password`, `chineseName`, `company`, `phoneNumber`, `userRight`, `secretKey`, `emailAddr`) VALUES 
+(1, 'rinoa', 'd41d8cd98f00b204e9800998ecf8427e', 'chine', 'checkit', '1865000000', 'low', 'd41d8cd98f00b204e9800998ecf8427e', 'rinoa@hotmail'),
+(2, 'seifer', '493603d84f0098e827896d5c08c81480', 'seifertest', 'seifer', '444444444', 'low', '493603d84f0098e827896d5c08c81480', 'seifer@hotmail.com'),
+(3, 'squall', 'f9ba7d6af07eecbbccd33510304aba1b', 'chine', 'VPT-TECH', '1555555555', 'super', 'secretKey1', 'squall@gmail'),
+(4, 'xiaowei', '12345666666666', 'xiaowei', 'tpv', '1234578', '123', 'ww', 'xiaowei@163.com'),
+(5, 'may', 'whatooggg', 'tete', '烟草', '1234568098', 'hhww', 'whtao', 'tete@126.com'),
+(6, 'lili', 'lilidouble', 'lili', 'IBM', '3453455', 'rrtyhjssj', 'fggghh', 'lili@qq.com'),
+(7, 'mike', 'mikenihao', '迈克', '微软', '13599746307', 'niwehtgfbbb', 'fbguhgbbmnm', 'mike@hotmail'),
+(8, 'jerry', 'jerrymima', '珍妮', '移动', '1234567', 'fgthghh', 'fhghnhhnhnn', 'jerry@qq.com'),
+(9, 'kate', 'kategnn', 'ngngjgnnb', '电信', 'wefbbnn', 'gghjhjhjhn', 'ghhjjjjjkk', 'kate@yahoo.cn');
